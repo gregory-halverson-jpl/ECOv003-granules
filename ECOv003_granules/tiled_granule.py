ef4bec4bcc39d56bfd6973b245bfcfdf51da16ec
Evan Davis <evan.w.davis@jpl.nasa.gov>
2025-09-08 10:50:48 -0700

    Assume input granules have no BuildID either

--- ECOv003_granules/tiled_granule.py
+++ ECOv003_granules/tiled_granule.py
@@ -412,28 +412,28 @@ class ECOSTRESSTiledGranule(ECOSTRESSGranule):
         if self._orbit is not None:
             return self._orbit
         else:
-            return int(self.granule_name.split('_')[-6])
+            return int(self.granule_name.split('_')[-5])
 
     @property
     def scene(self) -> int:
         if self._scene is not None:
             return self._scene
         else:
-            return int(self.granule_name.split('_')[-5])
+            return int(self.granule_name.split('_')[-4])
 
     @property
     def tile(self) -> str:
         if self._tile is not None:
             return self._tile
         else:
-            return self.granule_name.split('_')[-4]
+            return self.granule_name.split('_')[-3]
 
     @property
     def time_UTC(self) -> datetime:
         if self._time_UTC is not None:
             return self._time_UTC
         else:
-            return parser.parse(self.granule_name.split('_')[-3])
+            return parser.parse(self.granule_name.split('_')[-2])
 
     def UTC_to_solar(self, time_UTC: datetime, lon: float) -> datetime:
         return time_UTC + timedelta(hours=(np.radians(lon) / np.pi * 12))
@@ -465,7 +465,7 @@ class ECOSTRESSTiledGranule(ECOSTRESSGranule):
 
     @property
     def product(self) -> str:
-        return "_".join(self.granule_name.split("_")[1:-6])
+        return "_".join(self.granule_name.split("_")[1:-5])
 
     @property
     def granule_name(self) -> str:
